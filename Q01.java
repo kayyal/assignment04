87ab29889f1d7c4d49706dc140420c3f71bb9ef3
kayyal <amir.kayyal@gmail.com>
2019-03-16 00:18:34 +0330

    add constructor whith on string parameter.

--- Q01.java
+++ Q01.java
@@ -1,18 +1,23 @@
 package assignment04;
 
+import java.util.Scanner;
+
 public class Q01 {
-    String message ;
-    int number ;
+    String message;
+    int number;
 
+    public Q01(String message) {
+        this.message = message;
+    }
 
-
-    char encrypt (char inputChar , int addedNumber){
-        inputChar =(char)((int)(inputChar + addedNumber));
+    char encrypt(char inputChar, int addedNumber) {
+        inputChar = (char) ((int) (inputChar + addedNumber));
         return inputChar;
     }
-    String encryptAll (String message , int addedNumber){
-        char [] stringToChar = message.toCharArray();
-        for (char selectedchar:stringToChar) {
+
+    String encryptAll(String message, int addedNumber) {
+        char[] stringToChar = message.toCharArray();
+        for (char selectedchar : stringToChar) {
             stringToChar[selectedchar] = encrypt(selectedchar, addedNumber);
         }
         message = stringToChar.toString();
@@ -22,7 +27,8 @@ public class Q01 {
     public String getMessage() {
         return message;
     }
-    public void getCode (int code){
+
+    public void getCode(int code) {
         this.number = code;
     }
 }
