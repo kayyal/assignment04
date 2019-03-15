0d270de2087333234c36c0bac63609c98942b919
kayyal <amir.kayyal@gmail.com>
2019-03-16 00:19:05 +0330

    complete the class tor test.

--- MainforQ01.java
+++ MainforQ01.java
@@ -1,4 +1,15 @@
 package assignment04;
 
+import java.util.Scanner;
+
 public class MainforQ01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String mess = scanner.nextLine();
+        Q01 obj = new Q01(mess);
+        int number = scanner.nextInt();
+        System.out.println( obj.encryptAll(mess , number));
+
+
+    }
 }
