7b4fc04f10b1586d5bdb136d96676cc31728afe7
kayyal <amir.kayyal@gmail.com>
2019-03-15 22:38:35 +0330

    adds encrypt and encryptAll methods

---
+++ Q01.java
@@ -0,0 +1,15 @@
+package assignment04;
+
+public class Q01 {
+    String message ;
+    int number ;
+
+
+
+    char encrypt (char inputChar){
+        return 'c';
+    }
+    char encryptAll (String message){
+        return 'a';
+    }
+}
