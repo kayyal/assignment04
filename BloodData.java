826b2ffb3d5bdd23bcf6012b2793fc35ae4687fe
kayyal <amir.kayyal@gmail.com>
2019-03-16 18:44:45 +0330

    adds setter an getter for both fields.

--- BloodData.java
+++ BloodData.java
@@ -3,13 +3,30 @@ package assignment04;
 public class BloodData {
     String bloodType;
     String rhFactor;
-    public BloodData (){
+
+    public BloodData() {
         this.bloodType = "O";
         this.rhFactor = "Positive";
     }
 
     public BloodData(String bloodType, String rhFactor) {
         this.bloodType = bloodType;
+        this.rhFactor = rhFactor;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
+    }
+
+    public String getRhFactor() {
+        return rhFactor;
+    }
+
+    public void setRhFactor(String rhFactor) {
         this.rhFactor = rhFactor;
     }
 }
