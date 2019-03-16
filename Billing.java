64dc1c31efb308b2c85977b66c08c9e6610c21b1
kayyal <amir.kayyal@gmail.com>
2019-03-16 18:08:02 +0330

    adss main method for testing three overloaded computeBill.

--- Billing.java
+++ Billing.java
@@ -1,6 +1,12 @@
 package assignment04;
 
 public class Billing {
+    public static void main(String[] args) {
+        Billing billing = new Billing();
+        System.out.println(billing.computeBill(100));
+        System.out.println(billing.computeBill(100,10));
+        System.out.println(billing.computeBill(100,10,200));
+    }
     double computeBill(double price) {
         return price + price * 0.08;
     }
@@ -9,7 +15,7 @@ public class Billing {
         return ((price + price * 0.08) * quantity);
     }
     double computeBill (double price, int quantity , double coupon){
-        return  (price  * quantity - coupon)*0.92;
+        return  (price  * quantity - coupon)*1.08;
     }
 
 }
