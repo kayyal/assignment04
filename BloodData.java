a02e8c014daf7c50811bd7f2631ffd773deb7bb1
kayyal <amir.kayyal@gmail.com>
2019-03-16 18:38:57 +0330

    adds default constructor.

--- BloodData.java
+++ BloodData.java
@@ -1,4 +1,12 @@
 package assignment04;
 
 public class BloodData {
+    String bloodType;
+    String rhFactor;
+    public BloodData (){
+        this.bloodType = "O";
+        this.rhFactor = "Positive";
+    }
+
+  
 }
