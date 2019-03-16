757515c3ba14af23f57cedc6ca80ba8f974871c2
kayyal <amir.kayyal@gmail.com>
2019-03-16 19:18:40 +0330

    Complete the body of main method.

--- TestBloodData.java
+++ TestBloodData.java
@@ -1,4 +1,21 @@
 package assignment04;
 
 public class TestBloodData {
+    public static void main(String[] args) {
+        BloodData bloodData = new BloodData();
+        printtheBloodType(bloodData);
+
+        BloodData bloodData1 = new BloodData("AB" , "NEAGATIVE");
+        printtheBloodType(bloodData1);
+        bloodData.setBloodType("A");
+        bloodData.setRhFactor("POSITIVE");
+        printtheBloodType(bloodData);
+
+    }
+    static void printtheBloodType (BloodData bl){
+        System.out.println( "bloodType is : " + bl.getBloodType()+
+                "rhFactor is : " + bl.getRhFactor());
+    }
+
+
 }
