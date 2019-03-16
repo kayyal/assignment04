f9df5be3ea745a74f2bf43f53bf077c9a71e4b49
kayyal <amir.kayyal@gmail.com>
2019-03-16 19:57:32 +0330

    cmplete class patient.

--- Patient.java
+++ Patient.java
@@ -1,4 +1,34 @@
 package assignment04;
 
 public class Patient {
+    BloodData bloodData;
+    private int id;
+    private int age;
+    public Patient(int id, int age, BloodData bloodData) {
+        this.id = id;
+        this.age = age;
+        this.bloodData = bloodData;
+    }
+
+    public Patient() {
+        this.id = 0;
+        this.age = 0;
+        this.bloodData.setRhFactor("POSITIVE");
+        this.bloodData.setBloodType("O");
+
+    }
+
+    public BloodData getBloodData() {
+        return bloodData;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
 }
