6437ee0e892c22a4cec022b0234415ceb58ff92c
kayyal <amir.kayyal@gmail.com>
2019-03-16 20:06:39 +0330

    continue to complete !

--- MainForPatient.java
+++ MainForPatient.java
@@ -1,5 +1,17 @@
 package assignment04;
 
-public class mainForPatient {
-    
+public class MainForPatient {
+    public static void main(String[] args) {
+        Patient patient = new Patient();
+        printPatient(patient);
+        BloodData bloodData  = new BloodData();
+        Patient patient1 = new Patient(14314, 23 ,bloodData );
+        printPatient(patient1);
+
+
+    }
+   public static void  printPatient(Patient patient){
+       System.out.println("default constructor \n " + "id :" + patient.getId()
+               + "age : " + patient.getAge() + "bloodType : " + patient.getBloodData());
+   }
 }
