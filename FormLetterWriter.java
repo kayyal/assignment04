48393b1aa85e29efecb31e3c5c8bdb9758c8d5a2
kayyal <amir.kayyal@gmail.com>
2019-03-16 17:19:50 +0330

    adds displaySalutation methods.

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -1,4 +1,10 @@
 package assignment04;
 
 public class FormLetterWriter {
+    void displaySalutation(String lastName){
+        System.out.println("Dear Mr " + lastName);
+    }
+    void displaySalutation (String firstName , String lastName){
+        System.out.println("greeting Dear " + firstName + " " + lastName);
+    }
 }
