61f27a4cc36035524da381f54be67c8a132e6bb5
kayyal <amir.kayyal@gmail.com>
2019-03-16 18:03:09 +0330

    adds three overloaded computeBill.

--- Billing.java
+++ Billing.java
@@ -1,4 +1,15 @@
 package assignment04;
 
 public class Billing {
+    double computeBill(double price) {
+        return price + price * 0.08;
+    }
+
+    double computeBill(double price, int quantity) {
+        return ((price + price * 0.08) * quantity);
+    }
+    double computeBill (double price, int quantity , double coupon){
+        return  (price  * quantity - coupon)*0.92;
+    }
+
 }
