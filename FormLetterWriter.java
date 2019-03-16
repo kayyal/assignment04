c8e4afea4a0315f56439f7b53b64474c9b203a09
kayyal <amir.kayyal@gmail.com>
2019-03-16 17:43:35 +0330

    adds main method to test overloaded methods.

--- FormLetterWriter.java
+++ FormLetterWriter.java
@@ -1,10 +1,23 @@
 package assignment04;
 
 public class FormLetterWriter {
-    void displaySalutation(String lastName){
-        System.out.println("Dear Mr " + lastName);
+    public static void main(String[] args) {
+            FormLetterWriter flw = new FormLetterWriter();
+            flw.displaySalutation("Amir");
+            flw.displaySalutation("Ali" , "Alavi");
     }
-    void displaySalutation (String firstName , String lastName){
-        System.out.println("greeting Dear " + firstName + " " + lastName);
+    void displaySalutation(String lastName) {
+        System.out.println("Dear Mr " + lastName +
+                "\n Thank you so much for taking the time to meet with me" +
+                " to discuss selling my handmade sweaters" +
+                " in your wonderful shop.");
     }
+
+    void displaySalutation(String firstName, String lastName) {
+        System.out.println("greeting Dear " + firstName + " " + lastName +
+                "\n Thank you so much for taking the time to meet with me" +
+                " to discuss selling my handmade sweaters" +
+                " in your wonderful shop.");
+    }
+
 }
